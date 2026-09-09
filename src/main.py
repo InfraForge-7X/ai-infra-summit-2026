47e4f25821a8a933f074e3b787e270f4aa67d2c6
Tsadok <adidomahukpeou@gmail.com>
2026-09-09 08:34:41 +0100

    feat: register routing API scaffold

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
+from src.api.routes.routing import router as routing_router
+
 app = FastAPI(title="AFRI-EDGE", version="0.1.0")
+app.include_router(routing_router)
 
 
 @app.get("/health")
